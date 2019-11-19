d19364209110773bd2eab060ad90644b305da4e7
lyaure <49193419+lyaure@users.noreply.github.com>
2019-11-19 12:18:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lyaure/puzzle15/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.lyaure.puzzle15;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity{
+    Switch music;
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.song);
+        mp.setLooping(true);
+
+        music = findViewById(R.id.switch_music);
+        music.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    mp.start();
+                }
+                else
+                    mp.pause();
+            }
+        });
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(music.isChecked())
+        {
+            //mp.pause();
+        }
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(music.isChecked())
+        {
+            //mp.start();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        MenuItem menuAbout = menu.add("About");
+        MenuItem menuExit = menu.add("Exit");
+
+        menuAbout.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                showAboutDialog();
+                return true;
+            }
+        });
+
+        menuExit.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                showExitDialog();
+                return true;
+            }
+        });
+        return true;
+    }
+
+    private void showAboutDialog()
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("About Puzzle 15");
+        alertDialog.setIcon(R.drawable.logo);
+        alertDialog.setMessage("This app implements the Game of Fifteen\n\nBy Lyaure & Samuel (c)");
+        alertDialog.show();
+    }
+
+    private void showExitDialog()
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Exit Puzzle 15");
+        alertDialog.setIcon(R.drawable.logo);
+        alertDialog.setMessage("Do you really want to exit?");
+        alertDialog.setCancelable(false);
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                mp.stop();
+                finish();  // destroy this activity
+            }
+        });
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+            }
+        });
+        alertDialog.show();
+    }
+
+    public void start_game(View view)
+    {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+}
